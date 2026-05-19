fec7d62c1fb8248f623fcd7cbd1d3bcbb2d660e2
anasmuhammed-2k25 <muhammedanas8727@gmail.com>
2026-05-19 22:31:02 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -59,10 +59,9 @@ app.put("/api/admin/users/:id/:action", updateUserStatus);
 app.delete("/api/admin/users/:id", deleteUser);
 
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
-  });
-}
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 export default app;
